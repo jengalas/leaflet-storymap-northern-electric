287a747d86ededd21208f225340ab8f0a73beea3
Jennifer Galas <jennifer.galas@scranton.edu>
2022-03-28 19:18:15 -0400

    Update Sheets API key

--- northern-electric/google-doc-url.js
+++ northern-electric/google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1LWM3tni-H6lU88naKN6XZ_iRUCtafIgzcPLH6Ru5Lq0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyA_agWgjaN7CBw8hdnQFcAzmlJg9fCWlhY';
